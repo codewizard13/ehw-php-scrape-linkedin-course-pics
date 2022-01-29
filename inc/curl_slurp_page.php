e62714eb4bf1f3662195bdd054ac5cde41a5968e
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-29 10:17:49 -0600

    UPDATE curl_slurp_page.php:
    - Include ehw_lib.php
    - Remove get_web_content() and get_img_urls() func code
      as they have been moved to ehw_lib.php
    - Comment out print_link_list() and make new version:
      Renamed as: show_img_gallery()
    - Remove all HTML in page except <section> tag

--- inc/curl_slurp_page.php
+++ inc/curl_slurp_page.php
@@ -17,6 +17,8 @@ File Purpose:
 -->
 
 <?php
+/* INCLUDES */
+include('ehw_lib.php');
 
 /* VARIABLES */
 $base_url = 'https://www.linkedin.com/learning';
@@ -31,36 +33,27 @@ $search_str = "object oriented php";
 $search_url = $base_url . '/search?keywords=' . rawurlencode($search_str);
 echo '<h3>$search_url: ' . $search_url . '</h3>';
 
-// // Slurp web page content and return html
-// function get_web_content($search_url) {
-    
-//     // Slurp page code with cURL
-//     $curl = curl_init();
-//     curl_setopt($curl, CURLOPT_URL, $search_url);
-//     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-//     return curl_exec($curl);
-// }
-
 $html = get_web_content($search_url);
 
 // Define regex for image urls
 $regex_img = '!https://media-exp1.licdn.com/dms/image/[^\s"]*!';
 
-// // Get and return array of image urls from web content
-// function get_img_urls($html, $reg) {
-//     // Find and return array of all image urls
-//     preg_match_all($reg, $html, $matches);
-
-//     // Get unique urls only
-//     $images = array_values(array_unique($matches[0]));
-//     return $images;
-// }
-
 $img_urls = get_img_urls($html, $regex_img);
 
-function print_link_list($urls) {
+// function print_link_list($urls) {
+//     $out = ''; // initialize output buffer
+//     $out .= '<ul>';
+//     for ($i=0; $i<count($urls); $i++) {
+//         $out .= "<li class='res-img-cont'>";
+//         $out .= "<img src='$urls[$i]' style='width: 100%'>";
+//         $out .= '</li>';
+//     }
+//     $out .= '</ul>';
+//     return $out;
+// }
+// $html = print_link_list($img_urls);
+
+function show_img_gallery($urls) {
     $out = ''; // initialize output buffer
     $out .= '<ul>';
     for ($i=0; $i<count($urls); $i++) {
@@ -71,23 +64,14 @@ function print_link_list($urls) {
     $out .= '</ul>';
     return $out;
 }
-$html = print_link_list($img_urls);
+$html = show_img_gallery($img_urls);
+
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <section class='ehw-curl-results'>
-        <?php echo $html; ?>
-    </section>
-</body>
-</html>
+
+<section class='ehw-curl-results'>
+    <?php echo $html; ?>
+</section>
 
 
