ac31e494582d89b1f10d9f7fe0b632a0cb2712e9
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-30 08:53:30 -0600

    UPDATE tmplt__results.php: Put list item IMG tags on separate lines

--- inc/tmplt__results.php
+++ inc/tmplt__results.php
@@ -16,21 +16,54 @@ File Purpose:
 <!--<section class="ehw-curl-results">-->
 <h2>OUTPUT RESULTS:</h2>
 <h3>$srch_url: https://www.linkedin.com/learning/search?keywords=object%20oriented%20javascript%20es6</h3>
+
 <ul>
-  <li class="res-img-cont">1: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHrK3_TAA5SMg/learning-public-crop_288_512/0/1595872229643?e=1643641200&amp;v=beta&amp;t=IhlIDhIp_pJCy5Ip8fk9mkLpZnLcDgjYleFppfjhUWQ" style="width: 100%"></li>
-  <li class="res-img-cont">2: <img src="https://media-exp1.licdn.com/dms/image/C560DAQGv2jZ7bIgGcw/learning-public-crop_288_512/0/1610728168219?e=1643641200&amp;v=beta&amp;t=OMs5-AMatmWqcdbxei4ZzOEF_SFFKuo4XxCv2mXrSy4" style="width: 100%"></li>
-  <li class="res-img-cont">3: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHVilhWY2KSFw/learning-public-crop_288_512/0/1568059279318?e=1643641200&amp;v=beta&amp;t=5qzL1rQHmNcrUKq3bVcNimgXkasTxxkmwVaqXiUwcGE" style="width: 100%"></li>
-  <li class="res-img-cont">4: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHGEmHS49qQxw/learning-public-crop_288_512/0/1591038530598?e=1643641200&amp;v=beta&amp;t=IVOnD5sNNB9AfHUa3JP7cdw50k1yjvev2yuG7hQzKSE" style="width: 100%"></li>
-  <li class="res-img-cont">5: <img src="https://media-exp1.licdn.com/dms/image/C4D0DAQG6dQ0gh0wiAw/learning-public-crop_288_512/0/1622221555885?e=1643641200&amp;v=beta&amp;t=pJCHTbpDZ5CccI6bIoxraXvZVUEhwCoZ7IymReZ-KOU" style="width: 100%"></li>
-  <li class="res-img-cont">6: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQG0jc7cAzGK8g/learning-public-crop_288_512/0/1567117556883?e=1643641200&amp;v=beta&amp;t=D53rA1kjjTPae6SlWoGCSR-sYf33v9WYCIpuguu-YPY" style="width: 100%"></li>
-  <li class="res-img-cont">7: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQEL8HR59bH8XQ/learning-public-crop_288_512/0/1567117876141?e=1643641200&amp;v=beta&amp;t=rTU0uLKYQpIjZEoHzrmfbOXgP_-KpsEccM0qiLIYIBA" style="width: 100%"></li>
-  <li class="res-img-cont">8: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHZ5J3EiCOgAw/learning-public-crop_288_512/0/1600448853491?e=1643641200&amp;v=beta&amp;t=elRZnWY_xynggJbFXdUiU6Kq43ongYXtldGwdbyBizY" style="width: 100%"></li>
-  <li class="res-img-cont">9: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQF2DX4_GzNtiw/learning-public-crop_288_512/0/1567115269921?e=1643641200&amp;v=beta&amp;t=AjcMpZmUeSFC0rLXJuGupo0IUT3F3nTshHK_stkrKUQ" style="width: 100%"></li>
-  <li class="res-img-cont">10: <img src="https://media-exp1.licdn.com/dms/image/C4D0DAQHK-x5Xftt6NQ/learning-public-crop_288_512/0/1587058479577?e=1643641200&amp;v=beta&amp;t=UfNcMLhloUZZh8T6UA_PYkS_tXdpE4tViZx5yJj2VHU" style="width: 100%"></li>
-  <li class="res-img-cont">11: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQGtrR8LhK0msA/learning-public-crop_675_1200/0/1567118135233?e=1643641200&amp;v=beta&amp;t=fJ1s-0DLAyPZ_XXTeQFJOGxUA7LaQ8wTCmVUBTbC-pw" style="width: 100%"></li>
-  <li class="res-img-cont">12: <img src="https://media-exp1.licdn.com/dms/image/C560DAQGDHZ4AqQ0KYw/learning-public-crop_675_1200/0/1633633862125?e=1643641200&amp;v=beta&amp;t=4OO4rvQYij8yv3VFtTXRTnCNaik9Adwo_EdalSR-tlQ" style="width: 100%"></li>
-  <li class="res-img-cont">13: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQEKFN4RRiiQew/learning-public-crop_675_1200/0/1567117429279?e=1643641200&amp;v=beta&amp;t=nh1HJqHe14jnES9apMSA3kLsbe_apHWq4Rn9mC89OBY" style="width: 100%"></li>
-  <li class="res-img-cont">14: <img src="https://media-exp1.licdn.com/dms/image/C560DAQF-n4_FlFwbyw/learning-public-crop_675_1200/0/1598566200167?e=1643641200&amp;v=beta&amp;t=5XcnxzJRGBs4PzWjCIKY0RxNEPC07tQnZp5oz-yTEuU" style="width: 100%"></li>
-  <li class="res-img-cont">15: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHZxVXI0vqBhg/learning-public-crop_288_512/0/1633394027040?e=1643641200&amp;v=beta&amp;t=nlx5urihh36lRHJow0LNIDhPWt3La3nwGrnzfxJhaus" style="width: 100%"></li>
-  <li class="res-img-cont">16: <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHJsPGYk9eZ6Q/learning-public-crop_675_1200/0/1567118503268?e=1643641200&amp;v=beta&amp;t=sf2qf18w-tGdRgYietD0s3XJt7GmXTx2pRSMloZwqrk" style="width: 100%"></li>
+  <li class="res-img-cont">1: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHrK3_TAA5SMg/learning-public-crop_288_512/0/1595872229643?e=1643641200&amp;v=beta&amp;t=IhlIDhIp_pJCy5Ip8fk9mkLpZnLcDgjYleFppfjhUWQ" style="width: 100%">
+  </li>
+  <li class="res-img-cont">2: 
+    <img src="https://media-exp1.licdn.com/dms/image/C560DAQGv2jZ7bIgGcw/learning-public-crop_288_512/0/1610728168219?e=1643641200&amp;v=beta&amp;t=OMs5-AMatmWqcdbxei4ZzOEF_SFFKuo4XxCv2mXrSy4" style="width: 100%">
+  </li>
+  <li class="res-img-cont">3: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHVilhWY2KSFw/learning-public-crop_288_512/0/1568059279318?e=1643641200&amp;v=beta&amp;t=5qzL1rQHmNcrUKq3bVcNimgXkasTxxkmwVaqXiUwcGE" style="width: 100%">
+  </li>
+  <li class="res-img-cont">4: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHGEmHS49qQxw/learning-public-crop_288_512/0/1591038530598?e=1643641200&amp;v=beta&amp;t=IVOnD5sNNB9AfHUa3JP7cdw50k1yjvev2yuG7hQzKSE" style="width: 100%">
+  </li>
+  <li class="res-img-cont">5: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4D0DAQG6dQ0gh0wiAw/learning-public-crop_288_512/0/1622221555885?e=1643641200&amp;v=beta&amp;t=pJCHTbpDZ5CccI6bIoxraXvZVUEhwCoZ7IymReZ-KOU" style="width: 100%">
+  </li>
+  <li class="res-img-cont">6: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQG0jc7cAzGK8g/learning-public-crop_288_512/0/1567117556883?e=1643641200&amp;v=beta&amp;t=D53rA1kjjTPae6SlWoGCSR-sYf33v9WYCIpuguu-YPY" style="width: 100%">
+  </li>
+  <li class="res-img-cont">7: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQEL8HR59bH8XQ/learning-public-crop_288_512/0/1567117876141?e=1643641200&amp;v=beta&amp;t=rTU0uLKYQpIjZEoHzrmfbOXgP_-KpsEccM0qiLIYIBA" style="width: 100%">
+  </li>
+  <li class="res-img-cont">8: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHZ5J3EiCOgAw/learning-public-crop_288_512/0/1600448853491?e=1643641200&amp;v=beta&amp;t=elRZnWY_xynggJbFXdUiU6Kq43ongYXtldGwdbyBizY" style="width: 100%">
+  </li>
+  <li class="res-img-cont">9: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQF2DX4_GzNtiw/learning-public-crop_288_512/0/1567115269921?e=1643641200&amp;v=beta&amp;t=AjcMpZmUeSFC0rLXJuGupo0IUT3F3nTshHK_stkrKUQ" style="width: 100%">
+  </li>
+  <li class="res-img-cont">10: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4D0DAQHK-x5Xftt6NQ/learning-public-crop_288_512/0/1587058479577?e=1643641200&amp;v=beta&amp;t=UfNcMLhloUZZh8T6UA_PYkS_tXdpE4tViZx5yJj2VHU" style="width: 100%">
+  </li>
+  <li class="res-img-cont">11: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQGtrR8LhK0msA/learning-public-crop_675_1200/0/1567118135233?e=1643641200&amp;v=beta&amp;t=fJ1s-0DLAyPZ_XXTeQFJOGxUA7LaQ8wTCmVUBTbC-pw" style="width: 100%">
+  </li>
+  <li class="res-img-cont">12: 
+    <img src="https://media-exp1.licdn.com/dms/image/C560DAQGDHZ4AqQ0KYw/learning-public-crop_675_1200/0/1633633862125?e=1643641200&amp;v=beta&amp;t=4OO4rvQYij8yv3VFtTXRTnCNaik9Adwo_EdalSR-tlQ" style="width: 100%">
+  </li>
+  <li class="res-img-cont">13: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQEKFN4RRiiQew/learning-public-crop_675_1200/0/1567117429279?e=1643641200&amp;v=beta&amp;t=nh1HJqHe14jnES9apMSA3kLsbe_apHWq4Rn9mC89OBY" style="width: 100%">
+  </li>
+  <li class="res-img-cont">14: 
+    <img src="https://media-exp1.licdn.com/dms/image/C560DAQF-n4_FlFwbyw/learning-public-crop_675_1200/0/1598566200167?e=1643641200&amp;v=beta&amp;t=5XcnxzJRGBs4PzWjCIKY0RxNEPC07tQnZp5oz-yTEuU" style="width: 100%">
+  </li>
+  <li class="res-img-cont">15: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHZxVXI0vqBhg/learning-public-crop_288_512/0/1633394027040?e=1643641200&amp;v=beta&amp;t=nlx5urihh36lRHJow0LNIDhPWt3La3nwGrnzfxJhaus" style="width: 100%">
+  </li>
+  <li class="res-img-cont">16: 
+    <img src="https://media-exp1.licdn.com/dms/image/C4E0DAQHJsPGYk9eZ6Q/learning-public-crop_675_1200/0/1567118503268?e=1643641200&amp;v=beta&amp;t=sf2qf18w-tGdRgYietD0s3XJt7GmXTx2pRSMloZwqrk" style="width: 100%">
+  </li>
 </ul>
