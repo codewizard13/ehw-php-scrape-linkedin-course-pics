fb6a6f6d48c76dc3be68df61154693ab8c1b35aa
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-30 08:28:16 -0600

    ADD tmplt__results.php

---
+++ inc/tmplt__results.php
@@ -0,0 +1,16 @@
+<!--
+Project Name:   EHW PHP Demo: Scrape LinkedIn Course List Images
+Main Prj File:  controller.php
+
+This Filename:  tmplt__results.php
+Date Created:   01/30/22
+Date Updated:   --
+Programmer:     Eric L. Hepperle
+
+File Version:    1.00.00
+
+File Purpose:
+  HTML View Page template for what output should look like
+-->
+
+<?php
