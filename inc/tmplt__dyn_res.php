9b6bcb5a9ad1619c7ef331ec12724d2f6a816981
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-31 13:42:51 -0600

    UPDATE tmplt__dyn_res.php: Refactor vars. Change H3 to INPUT. Add input style

--- inc/tmplt__dyn_res.php
+++ inc/tmplt__dyn_res.php
@@ -14,18 +14,29 @@ File Purpose:
   This
 -->
 
-
+<style>
+input#srch_url {
+    width: 100%;
+    border-color: blueviolet;
+    height: 1.8rem;
+}
+</style>
 
 <!--<section class="ehw-curl-results">-->
 <?php
-$html = <<<HTM
-<h2>OUTPUT RESULTS:</h2>
-<h3>\$srch_url: https://www.linkedin.com/learning/search?keywords=object%20oriented%20javascript%20es6</h3>
-HTM;
+/* VARIABLES */
 
 // Set dummy urls
 $def_img_url = 'pix/thumbs/1610728168219.jpg';
 $dummy_urls = array_fill(0, 12, $def_img_url);
-$html .= show_img_gallery($dummy_urls, 16);
+$dummy_srch_url = 'https://www.linkedin.com/learning/search?keywords=object%20oriented%20javascript%20es6';
+
+$head = <<<HTM
+<h2>OUTPUT RESULTS:</h2>
+<input type=text name='srch_url' id='srch_url' value=$dummy_srch_url><br>
+\$srch_url: https://www.linkedin.com/learning/search?keywords=object%20oriented%20javascript%20es6</h3>
+HTM;
+
+$html = $head . show_img_gallery($dummy_urls, 16);
 
 ?>
